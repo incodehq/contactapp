d682b6289449e48f14f98eabcb446066cc0d1676
Rosco Kalis <roscokalis@gmail.com>
2015-12-18 17:37:17 +0100

    Added a groups tab to the app

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -8,7 +8,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngResource', 'jett.i
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
                 cordova.plugins.Keyboard.disableScroll(true);
             }
-            
+
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
                 window.StatusBar.styleLightContent();
@@ -16,21 +16,21 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngResource', 'jett.i
         });
     })
 
-  
+
     .config(function($stateProvider, $urlRouterProvider) {
-    
+
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
     // Set up the various states which the app can be in.
     // Each state's controller can be found in controllers.js
     $stateProvider
-    
+
         .state('login', {
             url: '/login',
             templateUrl: 'templates/login.html',
             controller: 'LoginCtrl'
         })
-    
+
         // setup an abstract state for the tabs directive
         .state('tab', {
             cache: false,
@@ -38,7 +38,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngResource', 'jett.i
             abstract: true,
             templateUrl: 'templates/tabs.html'
         })
-    
+
         // Each tab has its own nav history stack:
         .state('tab.contacts', {
             cache: false,
@@ -55,12 +55,32 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngResource', 'jett.i
             url: '/contacts/:instanceId',
             views: {
                 'tab-contacts': {
-                templateUrl: 'templates/contact-detail.html',
-                controller: 'ContactDetailCtrl as ctrl'
+                    templateUrl: 'templates/contact-detail.html',
+                    controller: 'ContactDetailCtrl as ctrl'
+                }
+            }
+        })
+        .state('tab.groups', {
+            cache: false,
+            url: '/groups',
+            views: {
+                'tab-groups': {
+                    templateUrl: 'templates/tab-groups.html',
+                    controller: 'GroupsCtrl as ctrl'
+                }
+            }
+        })
+        .state('tab.group-detail', {
+            cache: false,
+            url: '/groups/:instanceId',
+            views: {
+                'tab-groups': {
+                    templateUrl: 'templates/group-detail.html',
+                    controller: 'GroupDetailCtrl as ctrl'
                 }
             }
         });
-    
+
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise(function ($injector, $location) {
             var $state = $injector.get("$state");
