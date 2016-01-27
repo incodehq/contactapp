58e4e85a6c727cec201fc0aa1ee4d4c63fc23472
Rosco Kalis <roscokalis@gmail.com>
2016-01-27 14:12:08 +0100

    Changed the IntegTest to a Unit Test

---
+++ backend/dom/src/test/java/domainapp/dom/group/ContactGroupTest.java
@@ -0,0 +1,54 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package domainapp.dom.group;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ContactGroupTest {
+
+    ContactGroup contactGroup;
+
+    @Before
+    public void setUp() throws Exception {
+        contactGroup = new ContactGroup();
+    }
+
+    public static class Change extends ContactGroupTest {
+
+        @Test
+        public void happyCase() throws Exception {
+            // given
+            String name = "New name";
+            String email = "New email";
+            String address = "New address";
+            String notes = "New notes";
+
+            // when
+            contactGroup.change(name, email, address, notes);
+            // then
+            assertThat(contactGroup.getName()).isEqualTo(name);
+            assertThat(contactGroup.getEmail()).isEqualTo(email);
+            assertThat(contactGroup.getAddress()).isEqualTo(address);
+            assertThat(contactGroup.getNotes()).isEqualTo(notes);
+        }
+
+    }
+
+}
