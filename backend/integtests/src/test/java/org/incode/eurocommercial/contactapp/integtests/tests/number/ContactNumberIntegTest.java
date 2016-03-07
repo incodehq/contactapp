94a278aa01406c905b2130c2d3ade9cd4486005b
Sander Ginn <sanderginn@users.noreply.github.com>
2016-03-07 14:00:17 +0100

    ELI-83-increase-test-coverage: Some tests in ContactNumberIntegTest added

--- backend/integtests/src/test/java/org/incode/eurocommercial/contactapp/integtests/tests/number/ContactNumberIntegTest.java
+++ backend/integtests/src/test/java/org/incode/eurocommercial/contactapp/integtests/tests/number/ContactNumberIntegTest.java
@@ -22,13 +22,19 @@ import javax.inject.Inject;
 
 import org.junit.Before;
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import org.apache.isis.applib.fixturescripts.FixtureScripts;
 
+import org.incode.eurocommercial.contactapp.dom.number.ContactNumber;
 import org.incode.eurocommercial.contactapp.dom.number.ContactNumberRepository;
+import org.incode.eurocommercial.contactapp.dom.number.ContactNumberType;
 import org.incode.eurocommercial.contactapp.fixture.scenarios.demo.DemoFixture;
 import org.incode.eurocommercial.contactapp.integtests.tests.ContactAppIntegTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ContactNumberIntegTest extends ContactAppIntegTest {
 
@@ -38,34 +44,79 @@ public class ContactNumberIntegTest extends ContactAppIntegTest {
     @Inject
     ContactNumberRepository contactNumberRepository;
 
+    ContactNumber contactNumber;
+
     DemoFixture fs;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void setUp() throws Exception {
         // given
         fs = new DemoFixture();
         fixtureScripts.runFixtureScript(fs, null);
-    }
 
+        contactNumber = fs.getContacts().get(0).getContactNumbers().first();
+        nextTransaction();
+
+        assertThat(contactNumber).isNotNull();
+    }
 
     public static class Create extends ContactNumberIntegTest {
 
-        @Ignore("TODO")
         @Test
         public void add_number_with_existing_type() throws Exception {
+            // given
+            final String contactNumberType = ContactNumberType.OFFICE.title();
+            final String newOfficePhoneNumber = randomPhoneNumber();
+            assertThat(contactNumberRepository.listAll())
+                    .extracting(ContactNumber::getNumber)
+                    .doesNotContain(newOfficePhoneNumber);
 
+            // when
+            wrap(this.contactNumber).create(newOfficePhoneNumber, contactNumberType, null);
+            nextTransaction();
+
+            // then
+            assertThat(contactNumberRepository.listAll())
+                    .extracting(ContactNumber::getNumber)
+                    .contains(newOfficePhoneNumber);
         }
 
-        @Ignore("TODO")
         @Test
         public void add_number_with_new_type() throws Exception {
+            // given
+            final String contactNumberType = "New type";
+            final String newOfficePhoneNumber = randomPhoneNumber();
+            assertThat(contactNumberRepository.listAll())
+                    .extracting(ContactNumber::getNumber)
+                    .doesNotContain(newOfficePhoneNumber);
 
+            // when
+            wrap(this.contactNumber).create(newOfficePhoneNumber, null, contactNumberType);
+            nextTransaction();
+
+            // then
+            assertThat(contactNumberRepository.listAll())
+                    .extracting(ContactNumber::getNumber)
+                    .contains(newOfficePhoneNumber);
         }
 
-        @Ignore("TODO")
+        @Ignore
         @Test
         public void add_number_when_already_have_number_of_any_type() throws Exception {
+            // given
+            final String existingNumber = this.contactNumber.getNumber();
+            System.out.println(existingNumber);
 
+//            // then
+//            thrown.expect(ArrayIndexOutOfBoundsException.class);
+//            // TODO: Insert invalidation message
+//            thrown.expectMessage("");
+//
+//            // when
+//            wrap(this.contactNumber).create(existingNumber, ContactNumberType.OFFICE.title(), null);
         }
 
         @Ignore("TODO")
@@ -155,6 +206,5 @@ public class ContactNumberIntegTest extends ContactAppIntegTest {
         }
 
     }
-
 
 }
