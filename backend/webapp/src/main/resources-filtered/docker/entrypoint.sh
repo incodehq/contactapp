5bf74d75b483c49e759360151d23b598516f759e
Marcus van Bergen <mbhjvanbergen@gmail.com>
2019-05-23 16:47:59 +0200

    Removes the logging.properties dependency for entrypoint

--- backend/webapp/src/main/resources-filtered/docker/entrypoint.sh
+++ backend/webapp/src/main/resources-filtered/docker/entrypoint.sh
@@ -13,14 +13,13 @@ if [ -s /run/secrets/*.context.xml ];
     echo "context.xml NOT FOUND, proceeding with default config"
 fi
 
-if [ -s /run/secrets/*.isis.properties ] && [ -s /run/secrets/*.logging.properties ];
+if [ -s /run/secrets/*.isis.properties ];
   then
     # Symlink isis.properties.
     ln -sf /run/secrets/*.isis.properties /run/conf/isis.properties
-    ln -sf /run/secrets/*.logging.properties /run/conf/logging.properties
-    echo "FOUND isis.properties and logging.properties."
+    echo "FOUND isis.properties"
   else
-    echo "isis.properties and/or logging.properties NOT FOUND, proceeding with default config"
+    echo "isis.properties NOT FOUND, proceeding with default config"
 fi
 
 # Running Catalina
